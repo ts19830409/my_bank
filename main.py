7ace61242ec7ee4731d648759963d522bd8050b1
Aleksey Tsoy <i2705@rambler.ru>
2025-11-04 19:46:40 +1000

    Added get_date function to widget.py module and improve test coverage

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-﻿from src.masks import get_mask_card_number, get_mask_account
+﻿#from src.masks import get_mask_card_number, get_mask_account
+from src.widget import mask_account_card, get_date
+
+
+def main ( ) :
+    print ( "=== Тестирование маскировки банковских данных ===" )
+
+    tests = [
+        "Maestro 1596837868705199" ,
+        "Счет 64686473678894779589" ,
+        "MasterCard 7158300734726758" ,
+        "Счет 35383033474447895560" ,
+        "Visa Classic 6831982476737658" ,
+        "Visa Platinum 8990922113665229" ,
+        "Visa Gold 5999414228426353" ,
+        "Счет 73654108430135874305"
+    ]
+
+    for test in tests :
+        result = mask_account_card(test)
+        print (f"Ввод: {test}")
+        print (f"Вывод: {result}")
+        print ()
+
+    print("=== Тестирование форматирования даты ===")
+    test_data = "2024-03-11T02:26:18.671407"
+    result_data = get_date(test_data)
+    print(f"Ввод:  {test_data}")
+    print(f"Вывод: {result_data}")
+
 
 if __name__ == "__main__":
-    print(get_mask_card_number("1234567890123456"))
-    print(get_mask_account("12345678901234567890"))
+    main()
+
+    #print(get_mask_card_number("1234567890123456"))
+    #print(get_mask_account("12345678901234567890"))
+
+
--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,8 @@
 from src.masks import get_mask_card_number, get_mask_account
+import re
+
 def mask_account_card(full_string: str) -> str:
+    """Функция обрабатывающая информацию о картах и счетах."""
     full_name = full_string.split()
     number = full_name[-1]
     if len(number) == 16:
@@ -13,5 +16,10 @@ def mask_account_card(full_string: str) -> str:
     else:
         return "Введены не корректные значения"
 
+def get_date(full_date_string: str) -> str:
+    """Функция изменения формата даты"""
+    full_date = re.split(r"T" ,full_date_string)
+    new_date = full_date[0].split("-")
+    return f"{new_date[2]}.{new_date[1]}.{new_date[0]}"
 
 
