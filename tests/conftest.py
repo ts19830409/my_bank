5ae5f0b7cf538680069af982b3f77537d746bf84
Tsoy Aleksey <i2705@rambler.ru>
2025-11-22 09:51:20 +1000

    Black and Flake8 tested

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,15 @@
 import pytest
 
+
 @pytest.fixture
 def operations_data():
     return [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
+
 
 @pytest.fixture
 def sort_data():
@@ -17,12 +19,14 @@ def sort_data():
         {"date": "2023-02-05", "name": "C"},
     ]
 
+
 @pytest.fixture
 def simple_data():
     return [
         {"date": "2023-01-01", "name": "A"},
         {"date": "2023-02-01", "name": "B"},
     ]
+
 
 @pytest.fixture
 def invalid_data():
