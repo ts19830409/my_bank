096eeb401be81d0b1672dff3b5b12788a4e7a472
Tsoy Aleksey <i2705@rambler.ru>
2025-12-07 16:20:28 +1000

    add log decorator for homework 10.4

---
+++ src/decorators.py
@@ -0,0 +1,23 @@
+def log(filename=None):
+	"""Декоратор для автоматически логирования работы функции"""
+	def decorator(func):
+		def wrapper(*args, **kwargs):
+			try:
+				result = func(*args, **kwargs)
+				msg = f"{func.__name__} ok"
+				if filename:
+					with open(filename, 'a', encoding='utf-8') as f:
+						f.write(msg + '\n')
+				else:
+					print(msg)
+				return result
+			except Exception as e:
+				msg = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+				if filename:
+					with open(filename, 'a', encoding='utf-8') as f:
+						f.write(msg + '\n')
+				else:
+					print(msg)
+				raise
+		return wrapper
+	return decorator
